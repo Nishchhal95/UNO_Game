676975279c41577e6c2a22b680d1c5696d716109
nishchhal-bakshi <nishchhal.bakshi@iicorporate.com>
2020-04-25 23:33:12 +0530

    Basic UI for main menu

---
+++ Assets/Scripts/MainMenuUIManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuUIManager : MonoBehaviour
+{
+    [Header("MainMenu")]
+    [SerializeField] private Button playButton;
+    [SerializeField] private TextMeshProUGUI welcomeText;
+    private const string WELCOME_TEXT_FORMAT = "Welcome {0}";
+
+    [Header("NoNameUI")]
+    [SerializeField] private TMP_InputField playerNameInputField;
+    [SerializeField] private Button playerNameSubmitButton;
+
+    [Header("PhotonUI")]
+    [SerializeField] private Button gameCreateButton;
+    [SerializeField] private TMP_InputField roomCodeInputField;
+    [SerializeField] private Button gameJoinButton;
+}
